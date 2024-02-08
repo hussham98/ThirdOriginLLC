d52f47132dbc18911a14a0115108aed99c11f286
hussham98 <124159568+hussham98@users.noreply.github.com>
2024-02-08 19:18:29 +0530

    Update test.py
    Minor changes

--- test.py
+++ test.py
@@ -2,32 +2,34 @@ import unittest
 from dict2 import Dict2
 
 class TestDict2(unittest.TestCase):
-    def test_store_and_retrieve(self):
+    def test_store_and_access(self):
         obj = Dict2()
         obj['a'] = 1
+        obj['b'] = 2
+        obj['c'] = 3
         self.assertEqual(obj['a'], 1)
+        self.assertEqual(obj['b'], 2)
+        self.assertEqual(obj['c'], 3)
 
     def test_custom_exception(self):
         obj = Dict2()
         with self.assertRaises(KeyError):
             val = obj['a']
 
-    def test_for_loop(self):
+    def test_iterate_keys(self):
         obj = Dict2()
         obj['a'] = 1
         obj['b'] = 2
         obj['c'] = 3
-        keys = []
-        for k in obj:
-            keys.append(k)
-        self.assertListEqual(keys, ['a', 'b', 'c'])
+        keys = [k for k in obj]
+        self.assertEqual(keys, ['a', 'b', 'c'])
 
-    def test_update_existing_key(self):
+    def test_non_existing_key(self):
         obj = Dict2()
         obj['a'] = 1
-        obj['a'] = 2
-        self.assertEqual(obj['a'], 2)
+        with self.assertRaises(KeyError):
+            val = obj['b']
 
 if __name__ == '__main__':
     unittest.main()
-  
+    
